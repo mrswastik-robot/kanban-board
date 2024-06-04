e93cbedfb4284735f22d1da8aacc1b5cc9fff574
swastik <swastikpatel29@gmail.com>
2024-06-04 18:13:27 +0530

    checkbox

--- components/Item/index.tsx
+++ components/Item/index.tsx
@@ -5,6 +5,7 @@ import { CSS } from '@dnd-kit/utilities';
 import clsx from 'clsx';
 
 import { Grip } from 'lucide-react';
+import { Checkbox } from '../ui/checkbox';
 
 type ItemsType = {
   id: UniqueIdentifier;
@@ -13,6 +14,14 @@ type ItemsType = {
 };
 
 const Items = ({ id, title, category }: ItemsType) => {
+
+  const categoryClass = {
+    Low: ' bg-yellow-500',
+    Medium: ' bg-orange-500',
+    High: ' bg-red-600',
+    Critical: ' bg-red-800',
+  }[category] || 'bg-gray-200';
+
   const {
     attributes,
     listeners,
@@ -35,7 +44,7 @@ const Items = ({ id, title, category }: ItemsType) => {
         transform: CSS.Translate.toString(transform),
       }}
       className={clsx(
-        'px-2 py-4 bg-white  w-full rounded-lg border-2 bg-card text-card-foreground shadow-sm hover:border-gray-300 cursor-pointer',
+        'px-2 py-4 bg-white relative w-full rounded-lg border-2  text-card-foreground shadow-sm hover:border-gray-300 cursor-pointer',
         isDragging && 'opacity-50',
       )}
     >
@@ -45,13 +54,21 @@ const Items = ({ id, title, category }: ItemsType) => {
           className="border p-2 text-xs rounded-xl shadow-lg hover:shadow-xl"
           {...listeners}
         >
+          <span className="sr-only">Grip</span>
           <Grip size={16} />
         </button>
       </div>
       
-      <div>
-       <span className={` text-black font-bold`}>{category}</span> {/* Add this line */}
-      </div>
+      {category && 
+      <div className={clsx('rounded-3xl p-1 w-[30%] items-center justify-center text-center mt-2 shadow-md', categoryClass)}>
+       <span className={` text-white font-normal text-sm`}>{category}</span>
+      </div>}
+
+      {category && 
+      <div className=' absolute right-4 bottom-4'>
+        <Checkbox className="mt-2"/>
+      </div>}
+
     </div>
   );
 };
